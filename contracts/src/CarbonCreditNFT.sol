f2f5df41569fc43c5a11cfb06235a709fd6d0d4a
choguun <visaruth.s@gmail.com>
2025-04-26 10:37:23 +0700

    update: carbon credit nft

--- contracts/src/CarbonCreditNFT.sol
+++ contracts/src/CarbonCreditNFT.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
 
 import {ERC721} from "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {ERC721Enumerable} from "openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import {ERC721URIStorage} from "openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import {AccessControl} from "openzeppelin-contracts/contracts/access/AccessControl.sol";
 import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol"; // Or Ownable2Step
@@ -9,11 +10,12 @@ import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol"; // 
 /**
  * @title CarbonCreditNFT
  * @dev ERC721 token representing a verifiable carbon credit.
+ * Includes Enumerable extension for easier frontend querying.
  * Minting is restricted to addresses with the MINTER_ROLE.
  * Burning is handled internally, likely triggered by the RetirementLogic contract.
  * Metadata is stored off-chain (e.g., IPFS) via tokenURI.
  */
-contract CarbonCreditNFT is ERC721, ERC721URIStorage, AccessControl, Ownable {
+contract CarbonCreditNFT is ERC721, ERC721Enumerable, ERC721URIStorage, AccessControl, Ownable {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     // Add a role or address specifically for burning via retirement logic
     address public retirementContractAddress;
@@ -32,6 +34,7 @@ contract CarbonCreditNFT is ERC721, ERC721URIStorage, AccessControl, Ownable {
     {
         _grantRole(DEFAULT_ADMIN_ROLE, initialOwner); // Grant admin role to deployer
         _grantRole(MINTER_ROLE, initialOwner);        // Grant minter role initially to deployer
+        _nextTokenId = 1; // Initialize token IDs to start from 1
     }
 
     /**
@@ -50,15 +53,6 @@ contract CarbonCreditNFT is ERC721, ERC721URIStorage, AccessControl, Ownable {
     }
 
     /**
-     * @dev Burns a token. Made internal because it should likely only be called
-     * by the RetirementLogic contract upon successful retirement.
-     * We override the public burn function to prevent accidental burning.
-     */
-    // function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) { // Removed due to override errors
-    //     super._burn(tokenId);
-    // }
-
-    /**
      * @dev Burns a token upon request from the authorized RetirementLogic contract.
      * @param tokenId The ID of the token to burn.
      */
@@ -72,7 +66,7 @@ contract CarbonCreditNFT is ERC721, ERC721URIStorage, AccessControl, Ownable {
         // We just need to ensure the RetirementLogic contract calls this.
         
         // Use the internal _burn function
-        _burn(tokenId);
+        _burn(tokenId); // Will call the overridden _update below
     }
 
     /**
@@ -84,12 +78,29 @@ contract CarbonCreditNFT is ERC721, ERC721URIStorage, AccessControl, Ownable {
         emit RetirementContractSet(_retirementContract);
     }
 
-    // The following functions are overrides required by Solidity.
+    // --- OVERRIDES ---
+
+    // Override _update to include ERC721Enumerable's logic
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    // Override _increaseBalance which is now required by ERC721Enumerable
+    function _increaseBalance(address account, uint128 amount)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, amount);
+    }
 
     function tokenURI(uint256 tokenId)
         public
         view
-        override(ERC721, ERC721URIStorage)
+        override(ERC721, ERC721URIStorage) // Keep ERC721URIStorage here
         returns (string memory)
     {
         return super.tokenURI(tokenId);
@@ -98,9 +109,20 @@ contract CarbonCreditNFT is ERC721, ERC721URIStorage, AccessControl, Ownable {
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721URIStorage, AccessControl)
+        // Add ERC721Enumerable to the override list
+        override(ERC721, ERC721Enumerable, ERC721URIStorage, AccessControl) 
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
     }
+    
+    // Override _burn is needed if you inherit from multiple extensions that override it
+    // However, since _update handles the core logic, explicitly overriding _burn might not be strictly
+    // necessary unless you have custom burn logic. Let's rely on _update for now.
+    // function _burn(uint256 tokenId) 
+    //     internal 
+    //     override(ERC721, ERC721URIStorage) // Add ERC721Enumerable if it also overrides _burn
+    // {
+    //     super._burn(tokenId);
+    // }
 } 
